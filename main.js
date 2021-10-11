51cbcee83321bf7684d4d0d9aa8609470594ccbc
JonasWalker <67254086+JonasWalker@users.noreply.github.com>
2021-10-10 20:59:51 -0500

    creating main2

--- main.js
+++ main.js
@@ -50,4 +50,158 @@ app.on('ready', function () {
       // this is passing this === file://dirname/mainWindow.html into load url
     })
   )
+
+  mainWindow.on('closed', function () {
+    app.quit()
+  })
+
+  // build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+  // Insert Menu
+  Menu.setApplicationMenu(mainMenu)
 })
+
+// Handle create add window
+function createAddWindow(_xwindow, _title) {
+  // Create new window
+  _xwindow = _xwindow + '.hteml'
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: _title,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  })
+
+  // Load html into Window
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, _xwindow),
+      protocol: 'file',
+      slashes: true,
+      // this is passing this === file://dirname/addWindow.html into load url
+    })
+  )
+
+  // Garbage collection handle
+  addWindow.on('close', function () {
+    addWindow = null
+  })
+}
+
+// Catch item:add
+ipcMain.on('item:add', function (e, item) {
+  mainWindow.webContents.send('item:add', item)
+  addWindow.close()
+})
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label: 'Add',
+    submenu: [
+      {
+        label: 'Add Income',
+        click() {
+          createAddWindow()
+        },
+      },
+      {
+        label: 'Add Mortgage',
+      },
+      {
+        label: 'Add Bill',
+        click() {
+          mainWindow.webContents.send('item:clear')
+        },
+      },
+      /*{
+        label: 'Quit',
+        // this is a short cut to close app. checks if mac or windows
+        accelerator: process.plateform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit()
+        },
+      },*/
+    ],
+    /*label: 'Clear',
+    submenu: [
+      {
+        label: 'Clear Income',
+        click() {
+          createAddWindow()
+        },
+      },
+      {
+        label: 'Clear Bill',
+        click() {
+          mainWindow.webContents.send('item:clear')
+        },
+      },
+      // {
+      //   label: 'Quit',
+      //   // this is a short cut to close app. checks if mac or windows
+      //   accelerator: process.plateform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+      //   click() {
+      //     app.quit()
+      //   },
+      // },
+    ],*/
+  },
+]
+
+mainMenuTemplate.push({
+  label: 'Clear',
+  submenu: [
+    {
+      label: 'Clear Income',
+      click() {
+        //createAddWindow()
+      },
+    },
+    {
+      label: 'Clear Mortgage',
+    },
+    {
+      label: 'Clear Bill',
+      click() {
+        //mainWindow.webContents.send('item:clear')
+      },
+    },
+    // {
+    //   label: 'Quit',
+    //   // this is a short cut to close app. checks if mac or windows
+    //   accelerator: process.plateform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+    //   click() {
+    //     app.quit()
+    //   },
+    // },
+  ],
+})
+
+// If mac, add empty object to menu
+if (process.plateform == 'darwin') {
+  mainMenuTemplate.unshift({})
+}
+
+// Add developer tools item if not in production
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        // this is a short cut to close app. checks if mac or windows
+        accelerator: process.plateform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools()
+        },
+      },
+      {
+        role: 'reload',
+      },
+    ],
+  })
+}
